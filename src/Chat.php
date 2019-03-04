cce19a7b34244a67da8640de6d79e8f384f657bb
Julio Gastelum <tjuliogastelum90@gmail.com>
2019-03-04 05:00:10 -0800

    chat app

---
+++ src/Chat.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Rat;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+}
+
+?>
